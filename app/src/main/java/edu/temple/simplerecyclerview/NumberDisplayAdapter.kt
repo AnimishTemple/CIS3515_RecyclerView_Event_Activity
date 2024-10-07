60693db1fba134e9ae15b8a7e6b97b0c415b51eb
AnimishTemple <animishtenneti@gmail.com>
2024-10-07 11:55:11 -0400

    Completed Step 1, set on click listener inside viewHolder

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -9,13 +9,22 @@ import androidx.recyclerview.widget.RecyclerView
 // Step 1: Implement click behavior
 
 // Step 1a: Accept lambda in constructor
-class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+class NumberDisplayAdapter (_numbers: Array<Int>, _callBack: (Int) -> Unit) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+
+    // Initialize attributes
+    private val numbers = _numbers
+    private val callBack = _callBack
 
     // Step 1b: Invoke lambda via onClickListener
     inner class NumberViewHolder (layout: View) : RecyclerView.ViewHolder (layout) {
         // enumerate views inside layout
         val textView = layout.findViewById<TextView>(R.id.textView)
-
+        init {
+            textView.setOnClickListener {
+                // adapter position is automatically tracked and can be used as index of number clicked
+                callBack(numbers[adapterPosition])
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
